03ebe5a6add2c8da0bdcc27eb37198c7fce4e444
simon-chirchir <cmonchir297@gmail.com>
2022-12-14 10:09:11 +0300

    code commit on homepage and login page

--- Designs/js/login.js
+++ Designs/js/login.js
@@ -22,3 +22,45 @@ function login(username, password) {
     }
   }
   
+
+
+  /// Api consumption
+
+  // Get the form element
+const form = document.getElementById('login-form');
+
+// Get the input elements for the username and password
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+// Listen for the form submission event
+form.addEventListener('submit', (e) => {
+  // Prevent the default form submission behavior
+  e.preventDefault();
+
+  // Get the username and password values from the input elements
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // Call the login API with the username and password
+  login(username, password).then((response) => {
+    // If the login was successful, redirect the user to the dashboard page
+    if (response.status === 200) {
+      window.location.href = '/dashboard';
+    }
+  });
+});
+
+// The login function that makes an API request to the login endpoint
+function login(username, password) {
+  return fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+}
